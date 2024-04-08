9546f5eb6fa3ebaf0dcda4909716a553086010f4
Leonardo <leonardo38361@gmail.com>
2024-04-08 01:21:10 -0300

    endpont list_severalUsers

--- src/main/java/com/firstExercise/controllers/UserController.java
+++ src/main/java/com/firstExercise/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.firstExercise.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,12 @@ public class UserController {
 		return "Hola mundo";
 	}
 
-	@RequestMapping(value = "persona")
+	@RequestMapping(value = "person")
 	public List<String> listPeople() {
 		return List.of("Leonardo", "Luciana", "Matias");
 	}
 	
-	@RequestMapping(value = "user")
+	@RequestMapping(value = "users")
 	public User listUsers() {
 		User user = new User();
 		
@@ -30,6 +32,50 @@ public class UserController {
 		
 		return user;
 		 
+	}
+	
+	@RequestMapping(value = "user/{id}")
+	public User getUserById(@PathVariable Long id) {
+		User user = new User();
+		
+		user.setId(id);
+		user.setName("Leonardo");
+		user.setSurname("Puebla");
+		user.setEmail("leonardo@gmail.com");
+		user.setPhone("2612589352");
+		user.setPassword("1234");
+		
+		return user;
+		
+	}
+	
+	@RequestMapping(value = "list/user")
+	public List<User> list_severalUsers() {
+		List<User> user = new ArrayList<>();
+		
+		User firstUser = new User();
+		
+		firstUser.setId(3L);
+		firstUser.setName("Leonardo");
+		firstUser.setSurname("Puebla");
+		firstUser.setEmail("leonardo@gmail.com");
+		firstUser.setPhone("2612589352");
+		firstUser.setPassword("1234");
+		
+		User secondUser = new User();
+		
+		secondUser.setId(4L);
+		secondUser.setName("Leonardo");
+		secondUser.setSurname("Puebla");
+		secondUser.setEmail("leonardo@gmail.com");
+		secondUser.setPhone("2612589352");
+		secondUser.setPassword("5678");
+		
+		user.add(firstUser);
+		user.add(secondUser);
+		
+		return user;
+		
 	}
 
 }
