6de0ca31d2fdb61fd5777e815f13c0fa8e2702b5
Leonardo <leonardo38361@gmail.com>
2024-04-08 00:06:30 -0300

    listar personas

--- src/main/java/com/firstExercise/controllers/UserController.java
+++ src/main/java/com/firstExercise/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.firstExercise.models.User;
+
 @RestController
 public class UserController {
 	public String mensaje() {
@@ -16,6 +18,18 @@ public class UserController {
 		return List.of("Leonardo", "Luciana", "Matias");
 	}
 	
-	
+	@RequestMapping(value = "user")
+	public User listUsers() {
+		User user = new User();
+		
+		user.setName("Leonardo");
+		user.setSurname("Puebla");
+		user.setEmail("leonardo38361@gmail.com");
+		user.setPhone("2612589352");
+		user.setPassword("1234");
+		
+		return user;
+		 
+	}
 
 }
