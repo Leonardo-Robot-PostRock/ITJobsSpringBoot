a8788919f4240f57d861c29d3cd6b58d20cc5278
Leonardo <leonardo38361@gmail.com>
2024-04-16 18:26:21 -0300

    implementación de metodo getUsers

--- src/main/java/com/firstExercise/dao/UserDaoImpl.java
+++ src/main/java/com/firstExercise/dao/UserDaoImpl.java
@@ -4,13 +4,20 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 import com.firstExercise.models.User;
 
+import jakarta.persistence.EntityManager;
+
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
+	private EntityManager entityManager;
+	
 	@Override
 	public List<User> getUsers() {
-		return null;
+		String query = "from User";
+		List<User> result = entityManager.createQuery(query).getResultList();
+		
+		return result;
 	}
 
 }
