0b89268fc33872e924f8cd67006d5fc00a552ce6
Leonardo <leonardo38361@gmail.com>
2024-05-03 23:28:42 -0300

    insertar y eliminar usuario por id

--- src/main/java/com/firstExercise/dao/UserDaoImpl.java
+++ src/main/java/com/firstExercise/dao/UserDaoImpl.java
@@ -3,7 +3,6 @@ package com.firstExercise.dao;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import com.firstExercise.models.User;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -19,6 +18,17 @@ public class UserDaoImpl implements UserDao {
 		List<User> result = entityManager.createQuery(query).getResultList();
 		
 		return result;
+	}
+	
+	@Override
+	public void deleteUserById(Long id) {
+		User user = entityManager.find(User.class, id);
+		entityManager.remove(user);
+	}
+
+	@Override
+	public void insertUser(User user) {
+		entityManager.persist(user);
 	}
 
 }
