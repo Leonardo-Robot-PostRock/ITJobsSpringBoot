700cdd08f0db2df6b7b4165b7c6f71eabd9b1d53
Leonardo <leonardo38361@gmail.com>
2024-05-03 23:27:23 -0300

    uso de métodos en controlador

--- src/main/java/com/firstExercise/controllers/UserControllerWithID.java
+++ src/main/java/com/firstExercise/controllers/UserControllerWithID.java
@@ -3,7 +3,10 @@ package com.firstExercise.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.firstExercise.dao.UserDao;
@@ -15,10 +18,20 @@ public class UserControllerWithID {
 	@Autowired
 	private UserDao userDao;
 
-	@RequestMapping(value = "api/users")
+	@RequestMapping(value = "api/users", method = RequestMethod.GET)
 	public List<User> getUsers() {
 		List<User> user = userDao.getUsers();
 
 		return user;
+	}
+	
+	@RequestMapping(value = "api/users/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") Long id) {
+		userDao.deleteUserById(id);
+	}
+	
+	@RequestMapping(value = "api/insertUser", method = RequestMethod.POST)
+	public void insert(@RequestBody User user) {
+		userDao.insertUser(user);
 	}
 }
