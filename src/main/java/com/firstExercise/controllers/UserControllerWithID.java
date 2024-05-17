97c7bede9040943e0c518d0f460173355009bc2f
Leonardo <leonardo38361@gmail.com>
2024-05-17 19:31:11 -0300

    nuevas configuraciones en properties y importación unica

--- src/main/java/com/firstExercise/controllers/UserControllerWithID.java
+++ src/main/java/com/firstExercise/controllers/UserControllerWithID.java
@@ -3,12 +3,7 @@ package com.firstExercise.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.firstExercise.dao.UserDao;
 import com.firstExercise.models.User;
 
