5b6b1161a7f971221827b8871143aede06a10494
Leonardo <leonardo38361@gmail.com>
2024-05-17 15:19:55 -0300

    se crean las implementaciones de 2 métodos

--- src/main/java/com/firstExercise/dao/UserDaoImpl.java
+++ src/main/java/com/firstExercise/dao/UserDaoImpl.java
@@ -2,33 +2,57 @@ package com.firstExercise.dao;
 
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.firstExercise.models.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-
 
 @Repository
 public class UserDaoImpl implements UserDao {
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	@Override
+	@Transactional(readOnly = true)
 	public List<User> getUsers() {
 		String query = "from User";
 		List<User> result = entityManager.createQuery(query).getResultList();
-		
+
 		return result;
 	}
-	
+
 	@Override
+	@Transactional
 	public void deleteUserById(Long id) {
 		User user = entityManager.find(User.class, id);
 		entityManager.remove(user);
 	}
 
 	@Override
-	public void insertUser(User user) {
+	@Transactional
+	public void addUser(User user) {
 		entityManager.persist(user);
+	}
+
+	@Override
+	@Transactional
+	public void updateUser(Long id, User user) {
+		User existingUser = entityManager.find(User.class, id);
+
+		if (existingUser != null) {
+			existingUser.setName(user.getName());
+			existingUser.setSurname(user.getSurname());
+			existingUser.setEmail(user.getEmail());
+			existingUser.setPhone(user.getPhone());
+			existingUser.setPassword(user.getPassword());
+		}
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public User getUserById(Long id) {
+		return entityManager.find(User.class, id);
 	}
 
 }
