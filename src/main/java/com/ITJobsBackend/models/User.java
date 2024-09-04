f6e8bf37c9bbec452a63130662bebb3149c8214f
Leonardo <leonardo38361@gmail.com>
2024-09-04 16:49:30 -0300

    se agrega lombok

--- src/main/java/com/ITJobsBackend/models/User.java
+++ src/main/java/com/ITJobsBackend/models/User.java
@@ -1,7 +1,9 @@
 package com.ITJobsBackend.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 public class User {
 
@@ -25,52 +27,5 @@ public class User {
 	@Column
 	private String password;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurname() {
-		return surname;
-	}
-
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 }
