216a20a6f601626d500f722aeb58ee2b64ade39e
Leonardo <leonardo38361@gmail.com>
2024-09-05 03:16:41 -0300

    actualización de controlador

--- src/main/java/com/ITJobsBackend/controllers/UserController.java
+++ src/main/java/com/ITJobsBackend/controllers/UserController.java
@@ -5,41 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.ITJobsBackend.dao.UserDao;
-import com.ITJobsBackend.models.User;
-
+import com.ITJobsBackend.dto.UserDTO;
+import com.ITJobsBackend.services.UserService;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserDao userDao;
+	private UserService userService;
 
-	@CrossOrigin(origins="http://localhost:3000")
-	@RequestMapping(value = "api/users", method = RequestMethod.GET)
-	public List<User> getUsers() {
-		List<User> user = userDao.getUsers();
-
-		return user;
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(value = "api/users")
+	public List<UserDTO> getUsers() {
+		return userService.getUsers();
 	}
 
-	@RequestMapping(value = "api/delete-user/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable("id") Long id) {
-		userDao.deleteUserById(id);
-	}
-
-	@RequestMapping(value = "api/add-user", method = RequestMethod.POST)
-	public void add(@RequestBody User user) {
-		userDao.addUser(user);
-	}
-
-	@RequestMapping(value = "api/update-user/{id}", method = RequestMethod.PUT)
-	public void update(@PathVariable("id") Long id, @RequestBody User user) {
-		userDao.updateUser(id, user);
-	}
-
-	@RequestMapping(value = "api/user/{id}", method = RequestMethod.GET)
-	public User getUser(@PathVariable("id") Long id) {
-		return userDao.findUserById(id);
-	}
 }
