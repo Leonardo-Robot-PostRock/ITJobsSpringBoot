5325396e6914e09b4e01e49682faf6c28c059610
Leonardo <leonardo38361@gmail.com>
2024-09-05 03:13:44 -0300

    cambio de nombre en clase

--- src/main/java/com/ITJobsBackend/ITJobsBackendApplication.java
+++ src/main/java/com/ITJobsBackend/ITJobsBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstExerciseApplication {
+public class ITJobsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstExerciseApplication.class, args);
+		SpringApplication.run(ITJobsBackendApplication.class, args);
 	}
 
 }
