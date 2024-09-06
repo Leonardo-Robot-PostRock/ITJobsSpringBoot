79457dc966045e73afbdd1025f997f6930d7b7a3
Leonardo <leonardo38361@gmail.com>
2024-09-06 07:02:09 -0300

    creación de roles repository

---
+++ src/main/java/com/ITJobsBackend/repository/RolesRepository.java
@@ -0,0 +1,9 @@
+package com.ITJobsBackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ITJobsBackend.domain.Roles;
+
+public interface RolesRepository extends JpaRepository<Roles, Integer>{
+
+}
